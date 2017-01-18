b89f715eee393c73dc46759f38766d43e87b0347
Okeys <Olegrus1100@gmail.com>
2017-01-18 14:57:44 +0530

    Revert "Google Pixel Animation"
    This reverts commit 6cdd4adaad925e94636f03e0466d30b50e90fa99.

--- config/common.mk
+++ config/common.mk
@@ -2,10 +2,6 @@ PRODUCT_BRAND ?= LineageOS
 
 TARGET_USE_SDCLANG := true
 
-WITH_DEXPREOPT := true
-
-PRODUCT_BOOTANIMATION := vendor/cm/prebuilt/common/bootanimation/bootanimation.zip
-
 PRODUCT_BUILD_PROP_OVERRIDES += BUILD_UTC_DATE=0
 
 ifeq ($(PRODUCT_GMS_CLIENTID_BASE),)
@@ -148,13 +144,16 @@ PRODUCT_PACKAGES += \
     AudioFX \
     CMFileManager \
     CMSettingsProvider \
+    CMUpdater \
     CMWallpapers \
     CyanogenSetupWizard \
+    Eleven \
     ExactCalculator \
     LiveLockScreenService \
     LockClock \
     Screencast \
     SoundRecorder \
+    Trebuchet \
     WallpaperPicker \
     WeatherProvider 
 
